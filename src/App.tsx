ec2d55ee49a8dbabf8ac47923b7b730f5117402b
GalaDriel <g_ala@naver.com>
2022-12-10 04:51:42 +0900

    Update: Components - Common - HackerFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './assets/scss/index.scss';
+import HackerFooter from './components/common/HackerFooter';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<HackerFooter />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/common/HackerFooter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../assets/scss/components/common/HackerFooter.scss';
+
+const HackerFooter = () => {
+	const getCurrentYear = () => {
+		return new Date().getFullYear();
+	};
+
+	return (
+		<footer>
+			<div className="footerWrap">
+				<h2>© {getCurrentYear()} LEE MOON SEOB</h2>
+				<a
+					href="https://github.com/galadriel91"
+					target="_blank"
+					className="xi-github"
+					rel="noreferrer"
+				>
+					&nbsp;
+				</a>
+			</div>
+		</footer>
+	);
+};
+
+export default HackerFooter;
