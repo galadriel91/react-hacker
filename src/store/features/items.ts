e1af1c8cb304a5becaa01512f3f62fae050b291f
GalaDriel <g_ala@naver.com>
2022-12-09 20:03:52 +0900

    Update: Setup eslint & prettier

--- src/store/features/items.ts
+++ src/store/features/items.ts
@@ -1,16 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 export const itemSlice = createSlice({
-    name: 'item',
-    initialState: {
-        title: 'React Todo'
-    },
-    reducers: {
-        
-    },
-})
+	name: 'item',
+	initialState: {
+		title: 'React Todo',
+	},
+	reducers: {},
+});
 
 // Action creators are generated for each case reducer function
 // export const { ADD_TODO, CHECK_TODO , DELETE_TODO , UPDATE_TODO} = itemSlice.actions
 
-export default itemSlice.reducer
+export default itemSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import itemReducer from './features/items'
+import { configureStore } from '@reduxjs/toolkit';
+import itemReducer from './features/items';
 
-export const store =  configureStore({
-    reducer: {
-        items: itemReducer,
-    },
-})
+export const store = configureStore({
+	reducer: {
+		items: itemReducer,
+	},
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
