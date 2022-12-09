02c9fcf0bb03c0de3a756e4832e3373d44dfa7da
GalaDriel <g_ala@naver.com>
2022-12-09 19:35:48 +0900

    Update: Define ReactRouterDom

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,18 @@ import reportWebVitals from './reportWebVitals';
 
 import { store } from './store/index';
 import { Provider } from 'react-redux'
-
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
