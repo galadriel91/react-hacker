8cba2acffea1913102afb775da474436856feca8
GalaDriel <g_ala@naver.com>
2022-12-11 02:16:35 +0900

    Update: Define types in UserItem

--- src/components/user/UserItem.tsx
+++ src/components/user/UserItem.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { UserItems } from '../../store/features/types';
+import { UserItems, PostsItem } from '../../store/features/types';
 import Style from '../../assets/scss/components/user/UserItem.module.scss';
 
 type ItemProps = {
-	item: UserItems;
+	item: UserItems | PostsItem;
 };
 const UserItem = ({ item }: ItemProps) => {
+	const isUserItems = (typeOfProp: UserItems | PostsItem): typeOfProp is UserItems => {
+		return 'about' in typeOfProp;
+	};
+
 	return (
 		<div>
 			<div className={Style.userWrap}>
 				<p className="xi-user-address" />
-				{item.karma ? (
+				{isUserItems(item) ? (
 					<div className={Style.userInfo}>
 						<h4>User : {item.id}</h4>
 						<span>Created : {item.created}</span>
 						<p>Karma : {item.karma}</p>
 					</div>
 				) : (
-					''
-					// <div className={Style.userInfo}>
-					// 	<h4>
-					// 		<Link to={`/user/${item.user}`}>User : {item.user}</Link>
-					// 	</h4>
-					// 	<p>Posted : {item.time_ago}</p>
-					// 	<span>Points : {item.points}</span>
-					// </div>
+					<div className={Style.userInfo}>
+						<h4>
+							<Link to={`/user/${item.user}`}>User : {item.user}</Link>
+						</h4>
+						<p>Posted : {item.time_ago}</p>
+						<span>Points : {item.points}</span>
+					</div>
 				)}
 			</div>
-			{item.about ? (
+			{isUserItems(item) && item.about ? (
 				<div className={Style.userDesc}>
 					<h3>About</h3>
 					<div dangerouslySetInnerHTML={{ __html: item.about }} />
