5a33ce2378edc23029bea042918ddb7eb07405e3
GalaDriel <g_ala@naver.com>
2022-12-13 20:45:24 +0900

    Update: Apply UserPage try catch

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import App from './App';
 
 import { store } from './store/index';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import HackerScroll from './components/common/HackerScroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<HackerScroll />
 			<App />
 		</Provider>
-	</BrowserRouter>,
+	</HashRouter>,
 );
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { FETCH_USER } from 'store/features/sync';
+import { UserItems } from 'store/features/types';
 import UserItem from 'components/user/UserItem';
 import Style from 'assets/scss/pages/UserPage.module.scss';
 
@@ -9,17 +10,27 @@ const UserPage = () => {
 	const user = useAppSelector(state => state.items.user);
 	const dispatch = useAppDispatch();
 	const params = useParams();
+	const navigate = useNavigate();
+
+	const handldeFetch = async () => {
+		try {
+			const response = await dispatch(FETCH_USER(params.id as string));
+			const payload = response.payload as UserItems;
+			if (payload === null) {
+				navigate('/404');
+			} else {
+				return true;
+			}
+		} catch (err) {}
+	};
 
 	useEffect(() => {
-		dispatch(FETCH_USER(params.id as string));
+		handldeFetch();
+		// dispatch(FETCH_USER(params.id as string));
 		document.title = `${params.id} | Hacker News`;
 	}, [params.id]);
 
-	return (
-		<div className={Style.userContainer}>
-			<UserItem item={user} />
-		</div>
-	);
+	return <div className={Style.userContainer}>{user === null ? '' : <UserItem item={user} />}</div>;
 };
 
 export default UserPage;
