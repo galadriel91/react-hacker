4bca3361a4daaedc5ef91e8e91042fa130267238
GalaDriel <g_ala@naver.com>
2022-12-13 20:53:52 +0900

    Update: HashRouter -> BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import App from './App';
 
 import { store } from './store/index';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import HackerScroll from './components/common/HackerScroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<HashRouter>
+	<BrowserRouter>
 		<Provider store={store}>
 			<HackerScroll />
 			<App />
 		</Provider>
-	</HashRouter>,
+	</BrowserRouter>,
 );
