c4dab0ada1d2f13b3773c7d1ea37c939192d729d
GalaDriel <g_ala@naver.com>
2023-03-08 15:35:05 +0900

    Update: Components - Common - HackerFooter

--- src/components/common/HackerFooter.tsx
+++ src/components/common/HackerFooter.tsx
@@ -1,23 +1,61 @@
 import React from 'react';
-import 'assets/scss/components/common/HackerFooter.scss';
+import logo from 'assets/images/HackerNews.svg';
+import Style from 'assets/scss/components/common/HackerFooter.module.scss';
 
 const HackerFooter = () => {
-	const getCurrentYear = () => {
-		return new Date().getFullYear();
-	};
-
 	return (
-		<footer>
-			<div className="footerWrap">
-				<h2>© {getCurrentYear()} LEE MOON SEOB</h2>
-				<a
-					href="https://github.com/galadriel91"
-					target="_blank"
-					className="xi-github"
-					rel="noreferrer"
-				>
-					<div>빈값</div>
-				</a>
+		<footer className={Style.footer}>
+			<div className={Style.footerWrap}>
+				<div className={Style.footerTop}>
+					<div className={Style.footerLeft}>
+						<h2>
+							<img src={logo} alt="HackerNews 로고" />
+						</h2>
+						<p>
+							News platform for <br />
+							modern IT
+						</p>
+						<span>© Copyright Hacker News</span>
+					</div>
+					<div className={Style.footerRight}>
+						<ul>
+							<li>
+								<h3>Company</h3>
+								<a href="#none">YC Blog</a>
+								<a href="#none">Contact</a>
+								<a href="#none">Press</a>
+								<a href="#none">People</a>
+								<a href="#none">Careers</a>
+							</li>
+							<li>
+								<h3>Programs</h3>
+								<a href="#none">YC Program</a>
+								<a href="#none">Startup School</a>
+								<a href="#none">Work at a Startup</a>
+								<a href="#none">Continuity</a>
+							</li>
+							<li>
+								<h3>Resources</h3>
+								<a href="#none">Startup Directory</a>
+								<a href="#none">Startup Library</a>
+								<a href="#none">Covid</a>
+								<a href="#none">Investors</a>
+							</li>
+							<li>
+								<h3>ETC</h3>
+								<a href="#none">Guidelines</a>
+								<a href="#none">FAQ</a>
+								<a href="#none">Api</a>
+								<a href="#none">Security</a>
+								<a href="#none">Legal</a>
+								<a href="#none">Apply to YC</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className={Style.footerBottom}>
+					<span>© Copyright Hacker News</span>
+				</div>
 			</div>
 		</footer>
 	);
