70e108496d888288595ed7c5a695fc3aecc23416
GalaDriel <g_ala@naver.com>
2023-03-09 18:00:35 +0900

    Update: Defein main title name

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,9 @@ import MainHero from 'components/main/MainHero';
 import MainSection from 'components/main/MainSection';
 
 const MainPage = () => {
+	useEffect(() => {
+		document.title = `Main | Hacker News `;
+	}, []);
 	return (
 		<div>
 			<MainHero />
