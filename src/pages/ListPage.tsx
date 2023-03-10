680c511e9c98540be2b3f9ab61986e05926e25e1
GalaDriel <g_ala@naver.com>
2023-03-10 22:27:20 +0900

    Update: Handle ListPage case

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ListItem from 'components/list/ListItem';
 import ListPagination from 'components/list/ListPagination';
 import ListTitle from 'components/list/ListTitle';
-import { INIT_INDEX, INIT_CURRENT } from 'store/features/common';
+import { INIT_INDEX, INIT_CURRENT, SET_CURRENT, SET_INDEXNUM } from 'store/features/common';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { useLocation } from 'react-router-dom';
 import { FETCH_LIST } from 'store/features/sync';
@@ -47,10 +47,30 @@ const ListPage = () => {
 	}, [getTitleName()]);
 
 	useEffect(() => {
+		if (Number(location.pathname[location.pathname.length - 1]) === 1) {
+			dispatch(INIT_INDEX());
+			dispatch(INIT_CURRENT());
+		}
+	}, [location.pathname]);
+
+	useEffect(() => {
 		handldeFetch();
 		const name = getTitleName();
 		document.title = `${name} | Hacker News `;
-	}, [location.pathname]);
+	}, []);
+
+	useEffect(() => {
+		if (Number(location.pathname[location.pathname.length - 1]) >= 10) {
+			dispatch(SET_INDEXNUM(9));
+			dispatch(SET_CURRENT());
+		} else if (Number(location.pathname[location.pathname.length - 1]) >= 7) {
+			dispatch(SET_INDEXNUM(6));
+			dispatch(SET_CURRENT());
+		} else if (Number(location.pathname[location.pathname.length - 1]) >= 4) {
+			dispatch(SET_INDEXNUM(3));
+			dispatch(SET_CURRENT());
+		}
+	}, []);
 
 	return (
 		<div>
