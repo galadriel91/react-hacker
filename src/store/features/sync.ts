010135dfec9a8a91110363a6cb64b6bb79d548bf
GalaDriel <g_ala@naver.com>
2023-05-25 14:53:02 +0900

    Update: Handle FETCH_LIST, FETCH_USER error

--- src/store/features/sync.ts
+++ src/store/features/sync.ts
@@ -3,11 +3,19 @@ import { fetchListItem, fetchUserItem, fetchPostItem } from '../../api';
 
 const FETCH_LIST = createAsyncThunk('item/fetchListByPath', async (path: string, thunkAPI) => {
 	const response = await fetchListItem(path);
+	if (typeof response.data == 'string') {
+		// 리스트의 데이터가 없을경우
+		window.location.replace('https://react-hacker.netlify.app/404');
+	}
 	return response.data;
 });
 
 const FETCH_USER = createAsyncThunk('item/fetchUserByid', async (path: string, thunkAPI) => {
 	const response = await fetchUserItem(path);
+	if (!response.data.id) {
+		// User id정보가 없을 경우 에러 페이지로 리플레이스
+		window.location.replace('https://react-hacker.netlify.app/404');
+	}
 	return response.data;
 });
 
